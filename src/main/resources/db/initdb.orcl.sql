2ac05afc56b040088bedf3976d1a7ff6cd029387
Galiya_Mussulmanbekova <Galiya_Mussulmanbekova@epam.com>
2019-11-29 16:19:07 +0600

    added scripts for initialization database

---
+++ src/main/resources/db/initdb.orcl.sql
@@ -0,0 +1,66 @@
+-- docker exec -it OracleDB bash -c "source /home/oracle/.bashrc; sqlplus /nolog"
+-- connect sys as sysdba          -- password: Oradoc_db1
+-- alter session set "_ORACLE_SCRIPT"=true;
+-- create user internetshop identified by password;
+-- GRANT ALL PRIVILEGES to internetshop;
+
+CREATE SEQUENCE auto_seq
+ START WITH 100000
+ INCREMENT BY   1
+ NOCACHE
+ NOCYCLE;
+
+create table goods
+(
+    ID          NUMBER(20) DEFAULT auto_seq.NEXTVAL,
+    NAME        VARCHAR2(128)  NOT NULL,
+    DESCRIPTION VARCHAR2(1024) NOT NULL,
+    COST        NUMBER(10, 2)  NOT NULL,
+    COUNT       NUMBER(5)      NOT NULL,
+    CONSTRAINT goods_pk PRIMARY KEY (ID)
+);
+
+create table users
+(
+    ID          NUMBER(20) DEFAULT auto_seq.NEXTVAL,
+    USERNAME    VARCHAR2(50)   NOT NULL UNIQUE,
+    PASSWORD    VARCHAR2(1024),
+    GENDER      VARCHAR2(50),
+    LOCALE      VARCHAR2(50),
+    CONSTRAINT users_pk PRIMARY KEY (ID)
+);
+
+create table photo_goods
+(
+    GOODS_ID    NUMBER(20) NOT NULL,
+    PHOTO       CLOB,
+    FOREIGN KEY (GOODS_ID) REFERENCES goods (ID) ON DELETE CASCADE
+);
+
+create table orders
+(
+    ID          NUMBER(20) DEFAULT auto_seq.NEXTVAL,
+    USER_ID     NUMBER(20) NOT NULL,
+    CREATED     TIMESTAMP DEFAULT SYSDATE,
+    STATUS      NUMBER(1) check (STATUS in (0, 1)),
+    CONSTRAINT order_pk PRIMARY KEY (ID),
+    FOREIGN KEY (USER_ID) REFERENCES users (ID) ON DELETE CASCADE
+);
+
+create table order_details
+(
+    ID          NUMBER(20) DEFAULT auto_seq.NEXTVAL,
+    ORDER_ID    NUMBER(20) NOT NULL,
+    GOODS_ID    NUMBER(20) NOT NULL,
+    COUNT       NUMBER(5),
+    COST        NUMBER(10, 2)  NOT NULL,
+    FOREIGN KEY (ORDER_ID) REFERENCES orders (ID) ON DELETE CASCADE,
+    FOREIGN KEY (GOODS_ID) REFERENCES goods (ID) ON DELETE CASCADE
+);
+
+create table authorities
+(
+    USER_ID     NUMBER(20) NOT NULL,
+    AUTHORITY   VARCHAR2(10) check (AUTHORITY in ('ROLE_USER', 'ROLE_ADMIN')),
+    FOREIGN KEY (USER_ID) REFERENCES users (ID) ON DELETE CASCADE
+)
