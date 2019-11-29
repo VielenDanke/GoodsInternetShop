efc3509d300e90b05cb0097aeab152602e10c27d
VielenDanke <vielendanke1991@gmail.com>
2019-11-29 11:57:32 +0600

    project start

---
+++ src/main/java/kz/epam/InternetShop/InternetShopApplication.java
@@ -0,0 +1,13 @@
+package kz.epam.InternetShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/epam/InternetShop/InternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.epam.InternetShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
