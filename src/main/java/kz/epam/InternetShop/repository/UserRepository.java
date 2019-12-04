833f3237b37ef8bd4bf94d9143f9c0e743399997
VielenDanke <vielendanke1991@gmail.com>
2019-12-04 11:22:26 +0600

    User repositories query for deleting/finding user

--- src/main/java/kz/epam/InternetShop/repository/UserRepository.java
+++ src/main/java/kz/epam/InternetShop/repository/UserRepository.java
@@ -4,10 +4,19 @@ import kz.epam.InternetShop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
+    List<User> findByUsernameLike(String usernameLike);
+
+    List<User> findByAddressLike(String addressLike);
+
+    List<User> findByFullNameLike(String fullNameLike);
+
+    void deleteByUsername(String username);
 }
