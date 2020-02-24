e51ec7f346f40a64f6c07f3ec2868d1b3582f293
Vladislav_Dankevich <Vladislav_Dankevich@epam.com>
2020-02-24 11:06:14 +0600

    a

--- src/main/java/kz/epam/InternetShop/util/ValidationUtil.java
+++ src/main/java/kz/epam/InternetShop/util/ValidationUtil.java
@@ -3,6 +3,7 @@ package kz.epam.InternetShop.util;
 import kz.epam.InternetShop.util.exception.NotFoundException;
 
 public class ValidationUtil {
+
     public static void checkNotFound(boolean found, String arg) {
         if (!found) {
             throw new NotFoundException(arg);
