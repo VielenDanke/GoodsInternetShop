43de3fd96af532a30a6a8097a7506b0711d13377
Vladislav_Dankevich <Vladislav_Dankevich@epam.com>
2020-02-24 12:17:03 +0600

    Exception correct

--- src/main/java/kz/epam/InternetShop/util/exception/BadRequestException.java
+++ src/main/java/kz/epam/InternetShop/util/exception/BadRequestException.java
@@ -1,8 +1,6 @@
 package kz.epam.InternetShop.util.exception;
 
-import kz.epam.InternetShop.security.oauth2.user.OAuth2UserInfo;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -10,9 +8,5 @@ public class BadRequestException extends RuntimeException {
 
     public BadRequestException(String message) {
         super(message);
-    }
-
-    public BadRequestException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
